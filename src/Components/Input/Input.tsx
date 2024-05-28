d79f7c174cda4003608cd9623feccf3dd6342b41
AleksandrEnin163 <alexenin163@gmail.com>
2024-05-28 22:03:50 +0200

    fixed eslint

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -4,7 +4,12 @@ import { Button } from '../../stories/Button/Button';
 function Input() {
   return (
     <div className={s.input_form}>
-      <input type="text" id="isin_input" name="isin-input" placeholder="Enter ISIN" />
+      <input
+        type="text"
+        id="isin_input"
+        name="isin-input"
+        placeholder="Enter ISIN"
+      />
       <Button label="Add" />
       <Button label="Add Valid ISIN" />
     </div>
