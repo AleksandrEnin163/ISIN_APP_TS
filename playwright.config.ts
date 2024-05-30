e783e243552688823ec3067b4004f10e01427d5d
Nikita <keddofilatov@gmail.com>
2024-05-30 19:27:44 +0200

    feat(playwright-tests): add tests infra

---
+++ playwright.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    // Browser options
+    headless: false,
+    // Base URL
+    baseURL: 'http://localhost:3000'
+    // Additional options
+  }
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import mockServer from 'vite-plugin-mock-server';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000
+  },
   plugins: [react(), mockServer({})]
 });
