39086c4bf0299ad6b2e94b81f7449645cc0496b9
AleksandrEnin163 <alexenin163@gmail.com>
2024-06-03 21:11:39 +0200

    feat #7,10,11

--- src/Pages/Stocks/Stocks.tsx
+++ src/Pages/Stocks/Stocks.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-function Stocks() {
+function Stocks({stocks}) {
   return (
-    <div>Stocks</div>
-  )
+    <TableContainer sx={{ marginTop: '4vh'}}>
+      <Table sx={{ '& td, & th': { fontSize: '1.5vw', padding: '1vh 4vw' } }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Index</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {stocks.map((stock) => (
+            <TableRow key={stock.id}>
+              <TableCell>
+                <Link to={`stocks/${stock.id}`} style={{textDecoration: 'none', color: 'inherit'}}>
+                  <img src={stock.image_url} 
+                  alt={stock.stock_name} 
+                  style={{ width: '30px', height: '30px', marginRight: '10px' }}
+                  />
+                  {stock.stock_name}
+                </Link>
+              </TableCell>
+              <TableCell>{stock.stock_price}</TableCell>
+              <TableCell>{stock.index}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default Stocks
