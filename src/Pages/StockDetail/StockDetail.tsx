f004e6e3b97371628f6b1afdba526e84efa8ecfe
Nikita <keddofilatov@gmail.com>
2024-06-06 19:44:57 +0200

    feat(architecture): refactoring

--- src/Pages/StockDetail/StockDetail.tsx
+++ src/Pages/StockDetail/StockDetail.tsx
@@ -1,47 +1,36 @@
-import React from 'react'
-import axios from 'axios';
-import { useParams } from 'react-router-dom'
+import React from 'react';
+
+import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
-
-
-
+import { Stock } from './components/Stock/Stock';
+import { useStockUi } from './hooks/useStockUI';
 
 function StockDetail() {
-  const id = useParams().id //я когда раньше использовал useParams, он мне возразал значение строкой, а сейчас приходит объект
-  async function fetchStockInfo(){
-    const { data } = await axios.get(`/api/stock/${id}`)
-    return data
-  }
-  const { data, isLoading, isError } = useQuery({ queryKey: ['stockInfo'], queryFn: fetchStockInfo })
-  console.log(data)
-  if(isLoading){
-    return <h3>Loading...</h3>
+  //я когда раньше использовал useParams, он мне возразал значение строкой, а сейчас приходит объект
+  const { data, loading, error } = useStockUi();
+
+  if (loading) {
+    return <h3>Loading...</h3>;
   }
 
-  if(isError){
-    return <h3>Mistake with data fetching</h3>
+  if (error) {
+    return <h3>Mistake with data fetching</h3>;
   }
 
-  if(!data){
-    return <h3>No data</h3>
+  if (!data) {
+    return <h3>No data</h3>;
   }
-
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '3vh', marginTop: '20vh'}}>
-      <img src={data.image_url} 
-           alt={data.stock_name} 
-           style={{ width: '100px', height: '100px' }} 
-      />
-      <h1>{data.stock_name}</h1>
-      <h2>{data.stock_price}</h2>
-      <div style={{display: 'flex', gap: '3vw', fontSize: '20px'}}>
-        <p>Id: {data.id}</p>
-        <p>Index: {data.index}</p>
-        <p>ISIN: {data.isin}</p>
-      </div>
-    </div>
-  )
+    <Stock
+      imageUrl={data.imageUrl}
+      name={data.name}
+      price={data.price}
+      id={data.id}
+      indexName={data.indexName}
+      isin={data.isin}
+    />
+  );
 }
 
-export default StockDetail
+export default StockDetail;
---
+++ src/Pages/StockDetail/components/Stock/Stock.tsx
@@ -0,0 +1,30 @@
+type Props = {
+  imageUrl: string;
+  name: string;
+  price: string;
+  id: string;
+  indexName: string;
+  isin: string;
+};
+
+export const Stock = ({ imageUrl, name, price, id, indexName, isin }: Props) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '3vh',
+        marginTop: '20vh'
+      }}>
+      <img src={imageUrl} alt={name} style={{ width: '100px', height: '100px' }} />
+      <h1>{name}</h1>
+      <h2>{price}</h2>
+      <div style={{ display: 'flex', gap: '3vw', fontSize: '20px' }}>
+        <p>Id: {id}</p>
+        <p>Index: {indexName}</p>
+        <p>ISIN: {isin}</p>
+      </div>
+    </div>
+  );
+};
